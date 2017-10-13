e24a89a5e3f59d04bfc94e7f1b6cc9d894eeec5c
Rodrigo Dumont <rlsdumont@gmail.com>
2017-10-13 17:24:48 -0300

    Add new env vars

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
+FROM docker:17.06.1-ce as static-docker-source
 FROM google/cloud-sdk
+
+COPY --from=static-docker-source /usr/local/bin/docker /usr/local/bin/docker
 
 RUN \
   apt-get update \
--- kube-deploy.sh
+++ kube-deploy.sh
@@ -9,9 +9,16 @@ gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 gcloud --quiet config set container/cluster ${GOOGLE_CLUSTER_NAME}
 gcloud --quiet container clusters get-credentials ${GOOGLE_CLUSTER_NAME}
 
-if [ ! -e "${CIRCLE_WORKING_DIRECTORY}/kube.yml" ]
+if [ -e kube.yml ]
 then
-    cat /scripts/kube-template.yml | envsubst > "${CIRCLE_WORKING_DIRECTORY}/kube.yml"
+    cat kube.yml | envsubst > kube2.yml
+    mv kube2.yml kube.yml
+else
+    cat /scripts/kube-template.yml | envsubst > kube.yml
 fi
 
-kubectl apply -f "${CIRCLE_WORKING_DIRECTORY}/kube.yml"
+docker build -t us.gcr.io/${GOOGLE_PROJECT_ID}/$CIRCLE_PROJECT_REPONAME:$CIRCLE_SHA1 .
+docker tag us.gcr.io/${GOOGLE_PROJECT_ID}/$CIRCLE_PROJECT_REPONAME:$CIRCLE_SHA1 us.gcr.io/${GOOGLE_PROJECT_ID}/$CIRCLE_PROJECT_REPONAME:latest
+gcloud docker -- push us.gcr.io/${GOOGLE_PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}
+
+kubectl apply -f kube.yml
