15bcb58735ca5e658586c127e881960d78955c6a
Gabriel Arrais <arraiscgabriel@gmail.com>
2017-11-17 14:52:16 -0200

    Fix image name in kube template

--- kube-env-deploy.sh
+++ kube-env-deploy.sh
@@ -10,8 +10,8 @@ ENV=${1:-development}
 echo export ENV=$ENV
 
 # Define deployment variables considering environment
-CLUSTER=${GOOGLE_DEVELOPMENT_CLUSTER_NAME}
-COMPUTE_ZONE=${GOOGLE_DEVELOPMENT_COMPUTE_ZONE}
+CLUSTER=${GOOGLE_CLUSTER_NAME_DEVELOPMENT}
+COMPUTE_ZONE=${GOOGLE_COMPUTE_ZONE_DEVELOPMENT}
 
 case "$ENV" in 
 "production")
@@ -51,8 +51,8 @@ fi
 
 # Create deploymeny
 docker build -t us.gcr.io/${GOOGLE_PROJECT_ID}/$CIRCLE_PROJECT_REPONAME:$CIRCLE_SHA1 .
-docker tag us.gcr.io/${GOOGLE_PROJECT_ID}/$CIRCLE_PROJECT_REPONAME:$CIRCLE_SHA1 us.gcr.io/${GOOGLE_PROJECT_ID}/$CIRCLE_PROJECT_REPONAME:$ENV
-gcloud docker -- push us.gcr.io/${GOOGLE_PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}:$ENV
+docker tag us.gcr.io/${GOOGLE_PROJECT_ID}/$CIRCLE_PROJECT_REPONAME:$CIRCLE_SHA1 us.gcr.io/${GOOGLE_PROJECT_ID}/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BRANCH
+gcloud docker -- push us.gcr.io/${GOOGLE_PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}:$CIRCLE_BRANCH
 
 # Apply deployment
 kubectl apply -f kube.yml
