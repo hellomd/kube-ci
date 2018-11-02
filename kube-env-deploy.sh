33928b3c8c386d18ce73ac769dc6ee9194e04388
Jonathan Cardoso Machado <me@jonathancardoso.com>
2018-11-02 16:47:11 -0300

    fix conditional

--- kube-env-deploy.sh
+++ kube-env-deploy.sh
@@ -68,7 +68,7 @@ fi
 
 # Apply deployment with linkerd proxy, unless production env
 services_with_linkerd=('marketplace' 'users' 'authorization' 'memberships')
-if [ "$ENV" = "development" && " ${services_with_linkerd[@]} " =~ " ${CIRCLE_PROJECT_REPONAME} " ] || [ "$ENV" = "staging" && " ${services_with_linkerd[@]} " =~ " ${CIRCLE_PROJECT_REPONAME} " ]; then
+if [[ "$ENV" = "development" && " ${services_with_linkerd[@]} " =~ " ${CIRCLE_PROJECT_REPONAME} " ]] || [[ "$ENV" = "staging" && " ${services_with_linkerd[@]} " =~ " ${CIRCLE_PROJECT_REPONAME} " ]]; then
   linkerd version
   cat kube.yml | linkerd inject - | kubectl apply -f -
 else
