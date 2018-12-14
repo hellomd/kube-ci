43cdb3345e397ae11de07b20bb388454003b1bd1
Jonathan Cardoso Machado <me@jonathancardoso.com>
2018-12-14 12:28:03 -0200

    move marketplace to new infra

--- kube-env-deploy.sh
+++ kube-env-deploy.sh
@@ -113,7 +113,7 @@ else
 fi
 
 # New infra with logging and apm on ELK stack
-services_with_new_infra=( 'playground' 'authorization' 'users' )
+services_with_new_infra=( 'playground' 'authorization' 'users' 'marketplace' )
 ENABLE_APM="0"
 ENABLE_STRUCTURED_LOGGING="0"
 # Enable above if service is already configured for new infra
