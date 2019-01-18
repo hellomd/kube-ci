6a168540153776a59e3c7177db93d9b1f5ece37e
Jonathan Cardoso Machado <me@jonathancardoso.com>
2019-01-18 17:41:01 -0200

    add payments to new infra

--- kube-env-deploy.sh
+++ kube-env-deploy.sh
@@ -116,7 +116,7 @@ else
 fi
 
 # New infra with logging and apm on ELK stack
-services_with_new_infra=( 'playground' 'authorization' 'users' 'marketplace' 'backend-for-frontend' 'packing-slip' 'funnel-pdf' )
+services_with_new_infra=( 'playground' 'authorization' 'users' 'marketplace' 'backend-for-frontend' 'packing-slip' 'funnel-pdf' 'payments' )
 ELASTIC_APM_ACTIVE="false"
 ENABLE_STRUCTURED_LOGGING="false"
 # Enable above if service is already configured for new infra
